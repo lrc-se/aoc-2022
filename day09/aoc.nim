5afec49dd7c8fd3dc078bfa4dbc1626d15523ad7
Kalle Brisland <kalle.brisland@cygni.se>
2022-12-09 08:52:22 +0100

    Day 9: solved puzzle

---
+++ day09/aoc.nim
@@ -0,0 +1,11 @@
+import std/[os, strutils]
+import puzzle
+
+let runPuzzle =
+  case getEnv("part"):
+    of "part1", "": runPartOne
+    of "part2": runPartTwo
+    else: quit "Unknown part"
+
+let filename = (if getEnv("mode") == "test": "input-test" else: "input") & ".txt"
+echo "Result: ", readFile(filename).strip(leading = false).splitLines.parseInput.runPuzzle
---
+++ day09/puzzle.nim
@@ -0,0 +1,55 @@
+import std/[strutils, sequtils, sets, math]
+
+type
+  Vector = object
+    x: int
+    y: int
+  Motion = object
+    delta: Vector
+    steps: int
+
+func parseMotion(line: string): Motion =
+  let parts = line.split(' ')
+  case parts[0]:
+    of "U": result.delta = Vector(x: 0, y: -1)
+    of "R": result.delta = Vector(x: 1, y: 0)
+    of "D": result.delta = Vector(x: 0, y: 1)
+    of "L": result.delta = Vector(x: -1, y: 0)
+
+  result.steps = parseInt(parts[1])
+
+func simulateRope(motions: seq[Motion]; knotCount: int): int =
+  var tailPositions = initHashSet[Vector]()
+  var headPos = Vector(x: 0, y: 0)
+  var knotPositions = newSeq[Vector](knotCount - 1)
+  for motion in motions:
+    for _ in 1 .. motion.steps:
+      headPos.x += motion.delta.x
+      headPos.y += motion.delta.y
+
+      var nextPos = headPos
+      for knotPos in knotPositions.mitems:
+        let deltaX = nextPos.x - knotPos.x
+        let deltaY = nextPos.y - knotPos.y
+        let dist = deltaX.abs + deltaY.abs
+        if dist > 1:
+          if deltaX == 0:
+            knotPos.y += deltaY.sgn
+          elif deltaY == 0:
+            knotPos.x += deltaX.sgn
+          elif dist > 2:
+            knotPos.x += deltaX.sgn
+            knotPos.y += deltaY.sgn
+
+        nextPos = knotPos
+
+      tailPositions.incl(knotPositions[^1])
+
+  result = tailPositions.card
+
+
+func parseInput*(lines: seq[string]): seq[Motion] = lines.map(parseMotion)
+
+func runPartOne*(input: seq[Motion]): int = simulateRope(input, 2)
+
+func runPartTwo*(input: seq[Motion]): int = simulateRope(input, 10)
